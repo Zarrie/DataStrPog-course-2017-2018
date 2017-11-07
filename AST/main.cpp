5a4eadaac6f9032ee0123467b37a9bb5e967013d
Zarrie <22859401+Zarrie@users.noreply.github.com>
2017-11-07 15:19:02 +0200

    Add files via upload

--- AST/main.cpp
+++ AST/main.cpp
@@ -1,9 +1,10 @@
-#include "AST.h"
+#include "SyntaxTree.h"
+
 
 int main(){
 
-	AST tree;
-	tree.parseExpression("(((5+3)*(4+4))+((3+1)*(1+1)))");
+	SyntaxTree tree;
+	tree.parseExpression("((((5+3)*(4/2))+((3+1)*(1+1)))+(((1+3)*(2+4))+((3*4)*(1+1))))");
 	std::cout << tree;
 
 	return 0;
