4fea1aeb73f5c668bf33594e5d2bfb198ec8e587
Zarrie <22859401+Zarrie@users.noreply.github.com>
2018-01-18 16:51:09 +0200

    Add files via upload

---
+++ Trie/main.cpp
@@ -0,0 +1,45 @@
+#include "dictionary.h"
+
+void dictionaryINSERTtest(){
+	dictionary d;
+	d.insert("banana");
+	d.insert("cocoa");
+	d.insert("coconut");
+	std::cout << d.find("coconut");
+	std::cout << d.find("coconuts");
+	std::cout << std::endl;
+
+	d.printTree();
+}
+
+void dictionaryREMOVEtest(){
+	dictionary d;
+	d.insert("banana");
+	d.insert("cocoa");
+	d.insert("coconut");
+	std::cout << d.find("coconut");
+	std::cout << d.find("coconuts");
+	d.remove("coconut");
+	std::cout << d.find("coconut");
+	std::cout << std::endl;
+	d.remove("badWordGiven");
+}
+
+void dictionaryCOPYandASSIGNEMENTtest(){
+	dictionary d;
+	d.insert("banana");
+	d.insert("cocoa");
+	d.insert("coconut");
+	std::cout << d.find("coconut");
+	dictionary d1 = d;
+	std::cout << d1.find("coconut");
+	d.remove("coconut");
+	std::cout << d1.find("coconut");
+}
+
+int main(){
+	//dictionaryINSERTtest();
+	//dictionaryREMOVEtest();
+	//dictionaryCOPYandASSIGNEMENTtest();
+	return 0;
+}
